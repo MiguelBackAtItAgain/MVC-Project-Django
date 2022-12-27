9f3f7a13c7c57054ac0003b6e54adf5aaadd6a94
= <miguelbritosuq@outlook.es>
2022-12-27 00:34:45 -0500

    Extra correction

--- mvc/eduware/models.py
+++ mvc/eduware/models.py
@@ -138,6 +138,7 @@ class Solution(models.Model):
     id = models.AutoField(primary_key=True)
     answer = models.CharField(max_length=1000)
     student_in_course = models.ForeignKey("StudentCourse", on_delete=models.CASCADE)
+    challenge = models.ForeignKey("Challenge", on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['id']
