fe32964097d4c06776e8839ff4cb7636023839e0
= <miguelbritosuq@outlook.es>
2023-01-08 13:57:44 -0500

    Last changes

--- mvc/eduware/urls.py
+++ mvc/eduware/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_view
 from . import views
 
 urlpatterns = [
-    path('login', views.loginUser, name="login"),
+    path('', views.loginUser, name="login"),
     path('logout', views.logoutUser, name="logout"),
     path('student/home', views.studentLogin, name='Student home page'),
     path('teacher/home', views.teacherLogin, name='Teacher home page'),
